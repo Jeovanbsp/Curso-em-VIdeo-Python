2bf592ab249fb6352802b10317c4d81f634494fd
jeovan <jeovanbispoo@gmail.com>
2025-07-08 20:28:35 -0300

    Desafio 002: Entrada de Nome e Mensagem Personalizada

---
+++ desafio002.py
@@ -0,0 +1,6 @@
+# Desafio 002 - Curso em Vídeo (Python)
+# Objetivo: Receber o nome do usuário e mostrar uma mensagem de boas-vindas.
+# Aprendizado: uso da função input() para entrada de dados e print() com f-strings.
+
+nome = input("Digite seu nome: ")
+print('É um prazer te conhecer, {}!'.format(nome))
