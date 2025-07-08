324eb2b3ec7114f84b2f4ee3e1b6bd6d430b2f25
jeovan <jeovanbispoo@gmail.com>
2025-07-08 20:23:29 -0300

    Desafio 001: Olá,Mundo! com explicação no Código

---
+++ desafio001.py
@@ -0,0 +1,6 @@
+# Desafio 001 - Curso em Vídeo (Python)
+# Objetivo: Escreva "Olá, Mundo!" na tela.
+# Aprendizado: uso do comando print() e sintaxe básica.
+# Comentário: Esse é o primeiro contato com a linguagem Python.
+
+print("Olá, Mundo!")
