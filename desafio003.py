9edf522f3754a5083135e5d1d9fedea39878d03a
jeovan <jeovanbispoo@gmail.com>
2025-07-08 20:36:59 -0300

    Desafio 003: Soma de Dois Números com Explicação

---
+++ desafio003.py
@@ -0,0 +1,9 @@
+# Desafio 003 - Curso em Vídeo (Python)
+# Objetivo: Ler dois números inteiros e mostrar a soma deles.
+# Aprendizado: Conversão de tipos com int(), entrada de dados, operação aritmética e print().
+
+n1 = int(input('Digite o Primeiro Número: '))
+n2 = int(input('Digite o Segundo Número:'))
+soma = n1 + n2
+print('A Soma Entre {} e {} é igual a {}'.format(n1, n2, soma))
+# Fim do Desafio 003
