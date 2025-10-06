c60d72dafbccd83ea5f98d7aef2de718989b8675
jeovan <jeovanbispoo@gmail.com>
2025-10-06 13:14:16 -0300

    Desafio 021: Tocando MP3

---
+++ desafio021.py
@@ -0,0 +1,7 @@
+#Desafio 021: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+input('Agora você escuta?') 
+pygame.event.wait()
